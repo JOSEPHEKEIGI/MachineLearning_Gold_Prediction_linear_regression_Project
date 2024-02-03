3016eb9e6e1ace4160c2888c0a3587a3d6e5406b
JOSEPHEKEIGI <Joseph.keigi2015@gmail.com>
2024-02-03 13:33:18 +0300

    load web deployment

--- App.py
+++ App.py
@@ -1,50 +1,66 @@
-
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn import metrics
-from prophet import Prophet
+from pyscript import app
 
-def GetDataAndClean(FilePath):
-    data = FilePath
+
+def GetData(filePath):
+    data = filePath
     DataView = pd.read_csv(data)
     data = DataView.drop('Unnamed: 0',axis=1)
-    data['Date'] = pd.to_datetime(data['Date'])
     data['Price_Spread'] = data['High'] - data['Low']
     return data
 
-
-def Run_Model(data):
-    Closed_model = Prophet()
-    data.rename(columns={'Date':'ds','Close':'y'},inplace=True)
-    Closed_model.fit(data)  
+def model(data):
     X = data[['Volume', 'Open', 'High', 'Low','Price_Spread']]
-    y = data['y']
+    y = data['Close']
     X_train,X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
     model = LinearRegression()
-    model = model.fit(X_train,y_train)
-    prediction = model.predict(X_test)
-    return prediction
+    model.fit(X_train,y_train)
+    return model.coef_ , model.intercept_
 
 
+data = GetData(r'C:\Users\Cecilia Mwaura\Documents\Gold_Prediction_Project\Assets\goldstock.csv')
+#print(model(data))
+attrib = model(data)
 
+def Model_equation(attrib, value):
+    # Y = MX + C
+    # Volume = Close * Coef + Intercept
+    # ['Volume', 'Open', 'High', 'Low','Price_Spread']
+    coef, intercept = attrib
+    return coef[value] , intercept
 
+features={
+    'Volume':0,
+    'Open':1,
+    'High':2,
+    'Low':3,
+    'Price_Spread':4
+}
+ 
 
+print(Model_equation(attrib,features['Volume']))
 
-
-
-
-# def printMetrices()
-#     print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_prediction))
-#     print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_prediction))
-#     print('Root Mean Squared Error:', metrics.mean_squared_error(y_test, y_prediction, squared=False))
 
 
     
-data = GetDataAndClean(r'C:\Users\Cecilia Mwaura\Documents\Gold_Prediction_Project\Assets\goldstock.csv')
-model = Run_Model(data)
+    
+    
+    
+
+ 
+
 
     
+
+    
+
+
+
+
+
+
+
 
 
