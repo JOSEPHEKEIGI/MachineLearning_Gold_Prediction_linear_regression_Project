cc519640e417a5f1d1b4e1391a6425c948953797
JOSEPHEKEIGI <Joseph.keigi2015@gmail.com>
2024-02-07 17:21:42 +0300

    Adjust

--- App.py
+++ App.py
@@ -1,36 +1,50 @@
-import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import streamlit as st
+
 
 
 def GetData(filePath):
-    data = pd.read_csv(filePath)
+    data = filePath
+    DataView = pd.read_csv(data)
+    data = DataView.drop('Unnamed: 0',axis=1)
     data['Price_Spread'] = data['High'] - data['Low']
     return data
 
-
 def model(data):
-    X = data[['Volume', 'Open', 'High', 'Low', 'Price_Spread']]
+    X = data[['Volume', 'Open', 'High', 'Low','Price_Spread']]
     y = data['Close']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train,X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
     model = LinearRegression()
-    model.fit(X_train, y_train)
-    return model.coef_, model.intercept_
+    model.fit(X_train,y_train)
+    return model.coef_ , model.intercept_
 
 
 def Model_equation(attrib, value):
+    # Y = MX + C
+    # Volume = Close * Coef + Intercept
+    # ['Volume', 'Open', 'High', 'Low','Price_Spread']
     coef, intercept = attrib
-    return coef[value], intercept
+    return coef[value] , intercept
 
+features={
+    'Volume':0,
+    'Open':1,
+    'High':2,
+    'Low':3,
+    'Price_Spread':4
+}
+ 
 
-st.title('Linear Regression Model')
+st.title('Linear Regression Gold_Prediction Model')
+
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
     data = GetData(uploaded_file)
 
-   
+    
     st.subheader('Data Preview')
     st.write(data.head())
 
@@ -48,7 +62,7 @@ if uploaded_file is not None:
     coef_value, intercept_value = Model_equation((coefficients, intercept), features[selected_feature])
     st.write(f'{selected_feature} = {coef_value} * Close + {intercept_value}')
 
-   
+    
     st.subheader('Predict Close Price')
     volume = st.number_input('Volume')
     open_price = st.number_input('Open Price')
@@ -57,3 +71,9 @@ if uploaded_file is not None:
     price_spread = high_price - low_price
     predicted_close = coef_value * volume + coef_value * open_price + coef_value * high_price + coef_value * low_price + coef_value * price_spread + intercept_value
     st.write(f'Predicted Close Price: {predicted_close}')
+
+
+
+
+
+
